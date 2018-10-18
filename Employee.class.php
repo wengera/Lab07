bbfd138cc12187b414fedb4ffbd6c2d3fdd15813
Wenger, Alex <Alex.Wenger@Brooks.com>
2018-10-18 10:50:55 -0400

    Employee Abstract Class

--- Employee.class.php
+++ Employee.class.php
@@ -11,6 +11,31 @@
  *
  * @author awenger
  */
-class Employee {
-    var $person = new Person();
+abstract class Employee {
+    private $person;
+    private $ssn;
+    private $employee_count;
+    
+    //TODO
+    function __construct($person, $ssn, $employee_count){
+        $this->person = $person;
+        $this->ssn = $ssn;
+        $this->employee_count = $employee_count;
+    }
+    
+    public function getPerson(){
+        return $this->person;
+    }
+    
+    public function getSSN(){
+        return $this->ssn;
+    }
+    
+    public function getEmployeeCount(){
+        return $this->employee_count;
+    }
+    
+    public function toString(){
+        print("[Employee] TODO ToString()");
+    }
 }
