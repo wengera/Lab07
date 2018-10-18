c0f4aff450fa6d074743339981c7c22b3dc7c798
Wenger, Alex <Alex.Wenger@Brooks.com>
2018-10-18 11:16:31 -0400

    Created CommissionEmployee

--- CommissionEmployee.class.php
+++ CommissionEmployee.class.php
@@ -11,6 +11,32 @@
  *
  * @author awenger
  */
-class CommissionEmployee {
-    //put your code here
+class CommissionEmployee extends Employee {
+    private $sales;
+    private $commission_rate;
+    
+    //TODO
+    function __construct($sales, $commission_rate, $person, $ssn, $employee_count){
+        $this->sales = $sales;
+        $this->commission_rate = $commission_rate;
+        $this->person = $person;
+        $this->ssn = $ssn;
+        $this->employee_count = $employee_count;
+    }
+    
+    public function getPaymentAmount(){
+        return $this->sales * $this->commission_rate;
+    }
+    
+    public function getSales(){
+        return $this->sales;
+    }
+    
+    public function getCommissionRate(){
+        return $this->commission_rate;
+    }
+    
+    public function toString(){
+        print("[CommissionEmployee] TODO ToString()");
+    }
 }
