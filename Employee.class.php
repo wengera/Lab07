691075068a57e9647322ac8e7252fbf0abdd209f
Wenger, Alex <Alex.Wenger@Brooks.com>
2018-10-18 10:26:14 -0400

    Push stuff

---
+++ Employee.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Employee
+ *
+ * @author awenger
+ */
+class Employee {
+    var $person = new Person();
+}
